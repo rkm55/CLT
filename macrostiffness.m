ab797cac2bf159c1e6490a0c2bc8e1c4f0a61e66
Ryan Melander <ryan.melander5@gmail.com>
2022-01-24 18:55:19 -0700

    ABD calculations

--- macrostiffness.m
+++ macrostiffness.m
@@ -1,4 +1,28 @@
-function [ABD] = macrostiffness(Q,Qbar,t,n)
-% -----------------------------------------
-
-
+function [A,B,D] = macrostiffness(Qbar,t,n)
+% A,B,D macrostiffness matrix calculations following the criteria for 
+% generally orthotropic laminates (eq. 7.47)
+    % A (laminate extensional stiffness) (Pa m)
+    A = zeros(3);
+    for i=1:n
+        A(1,1) = Qbar(1,1,i)*t(i) + A(1,1);
+        A(1,2) = Qbar(1,2,i)*t(i) + A(1,2);
+        A(2,2) = Qbar(2,2,i)*t(i) + A(2,2);
+        A(3,3) = Qbar(3,3,i)*t(i) + A(3,3);
+        A(1,3) = Qbar(1,3,i)*t(i) + A(1,3);
+        A(2,3) = Qbar(2,3,i)*t(i) + A(2,3);
+    end
+    A(2,1) = A(1,2);
+    % B (laminate coupling stiffnesses) (Pa m^2)
+    B = zeros(3);   % All B(i,j) = 0
+    % A (laminate bending stiffnesses) (Pa m^3)
+    D = zeros(3);
+    for i=1:n
+        D(1,1) = (1/12)*Qbar(1,1,i)*t(i)^3 + D(1,1);
+        D(1,2) = (1/12)*Qbar(1,2,i)*t(i)^3 + D(1,2);
+        D(2,2) = (1/12)*Qbar(2,2,i)*t(i)^3 + D(2,2);
+        D(3,3) = (1/12)*Qbar(3,3,i)*t(i)^3 + D(3,3);
+        D(1,3) = (1/12)*Qbar(1,3,i)*t(i)^3 + D(1,3);
+        D(2,3) = (1/12)*Qbar(2,3,i)*t(i)^3 + D(2,3);
+    end
+    D(2,1) = D(1,2);
+end
--- main.m
+++ main.m
@@ -24,18 +24,31 @@ clc; clear; close all;
 % Qbar = stiffness tensor in global frame for each layer
 [Q,Qbar,S] = Qcalc(n,E1,E2,G12,v12,theta);
 
-disp('AS/3501 carbon/epoxy')
+% disp('AS/3501 carbon/epoxy')
 % disp('S ply 1')
 % disp(S(:,:,1))
-% disp('Q (GPa) ply 1')
+% disp('Boron with LM matrix')
+% disp('Q (GPa)')
 % disp(Q(:,:,1)/1e9)
-disp(['Qbar (GPa), rotation ',num2str(theta),' degrees'])
-disp(Qbar(:,:,1)/1e9)
+% disp(['Qbar (GPa), rotation ',num2str(theta),' degrees'])
+% disp(Qbar(:,:,1)/1e9)
+
+%% Macro Stiffness Constants
+% A = laminate extensional stiffnesses (Pa m)
+% B = laminate coupling stiffnesses (Pa m^2)
+% A = laminate bending stiffnesses (Pa m^3)
+[A,B,D] = macrostiffness(Qbar,t,n);
+
+% disp('A (Pa m)')
+% disp(A)
+% disp('B (Pa m^2)')
+% disp(B)
+% disp('D (Pa m^3)')
+% disp(D)
 
 %% Applied Forces
 % [M,N] = appforces;
 
-%% Macro Stiffness Constants
-% [ABD] = macrostiffness(Q,Qbar,t,n);
+
 
 
