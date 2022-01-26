3349c72362c5649c6838f2fb7c7f116e740929b3
Ryan Melander <ryan.melander5@gmail.com>
2022-01-26 12:40:22 -0700

    z loop update macrostiffness

--- temp.m
+++ temp.m
@@ -1,8 +1,10 @@
 clear; clc; close all;
 
+% temp file for testing
+
 %% function --- 
- z = zeros(1,n+1);
-    z(1) = -sum(t,'all')/2;
-    for i = 2:n+1
-        z(i) = z(i-1)+t(i-1);
-    end
+% Determine Ef and Ex for [0/90/0]s and [90/0/90]s beams
+% Assume E1=35 GPa and E2=10 Gpa and that each ply is 0.5 mm thick
+
+
+
